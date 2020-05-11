0daa3903e06a3d3d99ab7e5c958048c596ce7f75
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2020-05-11 10:13:48 +0900

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireFunctionsModule, REGION } from '@angular/fire/functions';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularFireAnalyticsModule,
+    AngularFireFunctionsModule,
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebase),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [{ provide: REGION, useValue: 'asia-northeast1' }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBPTJ2tXgLKTbQRQ7Il0DX4jqKghLKG8ro',
+    authDomain: 'zoom-long.firebaseapp.com',
+    databaseURL: 'https://zoom-long.firebaseio.com',
+    projectId: 'zoom-long',
+    storageBucket: 'zoom-long.appspot.com',
+    messagingSenderId: '150701070167',
+    appId: '1:150701070167:web:a390e91468a0aa1d70d012',
+    measurementId: 'G-WWVNT053ST',
+  },
 };
 
 /*
