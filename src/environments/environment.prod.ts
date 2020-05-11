2dbb49f5af0cd6f0e46fe3258d51c8ae9c6a34ba
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2020-05-11 14:48:39 +0900

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBPTJ2tXgLKTbQRQ7Il0DX4jqKghLKG8ro',
+    authDomain: 'zoom-long.firebaseapp.com',
+    databaseURL: 'https://zoom-long.firebaseio.com',
+    projectId: 'zoom-long',
+    storageBucket: 'zoom-long.appspot.com',
+    messagingSenderId: '150701070167',
+    appId: '1:150701070167:web:a390e91468a0aa1d70d012',
+    measurementId: 'G-WWVNT053ST',
+  },
 };
